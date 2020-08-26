d1e523a81be86200d686cd32a60a1f83f0c6a889
AbdullahMourad <46674560+AbdullahMourad@users.noreply.github.com>
2020-08-26 15:45:56 +0800

    Add files via upload

---
+++ Abdullah Mourad (a.mourad2011@gmail.com)/Q1.cpp
@@ -0,0 +1,31 @@
+// using c programming language 
+// mainly there are 3 ways to solve this,  first by jumping one by one rocket until we reach the end 
+// or by jumping two by two until reach the end and that would mean that N (total numbers of rocks is even number)
+// finally, I think the best way is to mix the jumps is the most suitable  for this case
+
+#include <stdio.h>
+#include <stdlib.h>
+#define bolean A=jump one rock
+#define B=jump two rocks
+
+
+int main ()
+int N=numbers of rocks
+
+{
+	
+	scanf ("%d",& N)
+	
+	if (N<1) 
+	printf("you have reached the end");
+	
+	else if (N=1)
+	printf("presse A to jump one last rock");
+	
+	else if (N>1)
+	printf("presse B to jumb")
+	
+	
+}
+
+ 
